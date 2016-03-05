9e0a0f63b949a55487e3488ee166568d81331c82
Hossein Karkeh Abadi <hosseink@stanford.edu>
2016-03-04 19:44:16 -0800

    two layer CNN added (torch)

--- torch/main.lua
+++ torch/main.lua
@@ -6,9 +6,9 @@ require 'torch'
 require 'cunn'
 require 'cutorch'
 
-dataset_dir = '../datasets/tiny-imagenet-200/'
-train_input = dataset_dir .. 'train/'
-val_input = dataset_dir .. 'val/'
+
+train_input='/home/ubuntu/tiny-imagenet-200/train/'
+val_input='/home/ubuntu/tiny-imagenet-200/val/'
 local dtype = 'torch.CudaTensor'
 
 local Nt = 0
@@ -82,14 +82,19 @@ print("Test Set Loaded!")
 
 -- Defining The Model
 net = nn.Sequential()
-net:add (nn.SpatialConvolution(3,32,3,3))
+net:add (nn.SpatialConvolution(3,32,3,3,1,1,1,1))
+net:add (nn.SpatialBatchNormalization(32*64*64))
 net:add (nn.ReLU())
+net:add (nn.Dropout(0.5))
 
-net:add (nn.SpatialConvolution(32,32,3,3))
+net:add (nn.SpatialConvolution(32,32,3,3,1,1,1,1))
+net:add (nn.SpatialBatchNormalization(32*64*64))
 net:add (nn.ReLU())
+net:add (nn.Dropout(0.5))
 
-net:add (nn.View(32*60*60))
-net:add (nn.Linear(32*60*60,200))
+net:add (nn.View(32*64*64))
+net:add (nn.Linear(32*64*64,200))
+net:add (nn.BatchNormalization(200))
 net:add (nn.ReLU())
 
 net:add (nn.Linear(200,200))
@@ -103,13 +108,18 @@ crit:type(dtype)
 
 print("Network is Loaded!")
 
+local batch_size=200
+local alpha=0.001
+local num_epoch=5
+
+
 
 
 local function f(w)
 	assert(w == weights)
-        local r = math.random(0,10)
-	local X_batch = X_train[{{1,100},{},{},{}}]:clone()
-	local y_batch = y_train[{{1,100}}]:clone()
+	local batchInd=torch.LongTensor(batch_size):random(100000) 
+	local X_batch = X_train:index(1,batchInd):clone()
+	local y_batch = y_train:index(1,batchInd):clone()
 	local x = X_batch:type(dtype)
 	local y = y_batch:type(dtype)
 	
@@ -124,13 +134,49 @@ local function f(w)
 end
 
 
+local function eval(x,y,batchSize)
+	local NVal = x:size(1)
+	local NBatch = NVal / batchSize
+	local y_pred = torch.Tensor(NVal)
+	local correct = 0
+	for i=1,NBatch do 
+		local s = (i-1)*batchSize +1
+		local e = i*batchSize  
+		local X_batch = x[{{s,e},{},{},{}}]:clone()
+		local y_batch = y[{{s,e}}]:clone()
+		local xg = X_batch:type(dtype)
+		local yg = y_batch:type(dtype)
+		net:evaluate()
+		local scores = net:forward(xg)
+		net:training()
+		maxs, indices = torch.max(scores, 2)
+		correct = correct + (torch.eq(yg,indices):sum())
+	end
+	return (100 * correct / NVal)	
+end
+	
+
+
+
 print("Foward-Backward Function Loaded!")
 
-for i=1,20 do
+
+local num_iter = num_epoch * 100000 / batch_size
+local epoch=1
+for i=1,num_iter do
 	local state = {learningRate = 1e-3}
 	optim.adam(f, weights, state)
-	local l=f(weights)
-	print(l)
+	if ((i%50)==0) then 
+		local l=f(weights)
+		local acc =eval(X_val,y_val,batch_size)	
+		print(string.format("Iteration=%d Loss=%f  Validation Accuracy=%f",i,l,acc))
+	end
+	if(i% (100000 / batch_size) ==0) then 
+		print(string.format("epoch = %d",epoch))
+		epoch = epoch +1
+	end
+	
+
 end
 
 
